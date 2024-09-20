41b2074b12d2090ac0cc1ba57d9a8da09245ee91
Leo <leo2012.c@gmail.com>
2024-09-20 21:21:09 +0800

    0920

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
 
 // swiper
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiperBlogs", {
   slidesPerView: 1,
   slidesPerGroup: 1,
-  spaceBetween: 48,
+  spaceBetween: 24,
   navigation: {
     nextEl: ".swiper-next",
-    prevEl: ".swiper-prev"
+    prevEl: ".swiper-prev",
   },
   pagination: {
     el: ".swiper-pagination-custom",
-    type: "fraction"
+    type: "fraction",
   },
 
   breakpoints: {
     768: {
       slidesPerView: 2,
-      slidesPerGroup: 2
-    }
-  }
+      slidesPerGroup: 2,
+    },
+  },
+});
+
+const swiperPopular = new Swiper(".swiperPopular", {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 24,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
 });
